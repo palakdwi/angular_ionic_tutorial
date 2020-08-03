fecdb811bfe30e1b9a70222e3113e60422666e4e
palakdwi <palak.dwi@gmail.com>
2020-08-03 14:01:08 +0530

    init

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -36,9 +36,3 @@ export class RecipesService {
     };
   }
 
-  deleteRecipe(recipeId: string) {
-    this.recipes = this.recipes.filter((recipe) => {
-      return recipe.id !== recipeId;
-    });
-  }
-}
